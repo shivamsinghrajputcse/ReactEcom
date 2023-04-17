137f4bf2642b25704f7829673d655cd2cd285802
shivamsinghrajputcse <shivamsinghcse21@gmail.com>
2023-04-17 23:35:57 +0530

    inproving my website

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react'
-import NavBar from './pages/NavBar'
-import NonFiction from './pages/NonFiction'
-import Edited from './pages/Edited'
-import Reference from  './pages/Reference'
-import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom'
+// import React from 'react'
+import NavBar from './pages/NavBar';
+import NonFiction from './pages/NonFiction';
+import Edited from './pages/Edited';
+import Fiction from './pages/Fiction';
+import Reference from './pages/Reference';
+import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
 const App = () => {
     return (
         <div>
-            <NavBar>
-            <div className='BookType'>
-                <Router> <Link to= "./NonFiction">NonFiction</Link>
-                <Link to= "./Edited">Edited</Link>
-                <Link to= "./Reference">Reference</Link>
-                <Routes>
-                    <Route path='./NonFiction' Component={NonFiction}></Route>
-                    <Route path='./Edited' Component={Edited}></Route>
-                    <Route path='./Reference' Component={Reference}></Route>
-                </Routes></Router>
+            <div className='Headerbar'>
+                <NavBar>
+
+
+                </NavBar>
+                <div className='BookType'>
+                    <Router className="Headingrouter">
+                        <Link to="/NonFiction" className='HeadingLink'>NonFiction</Link>
+                        <Link to="/Edited" className='HeadingLink'>Edited</Link>
+                        <Link to="/Reference" className='HeadingLink'>Reference</Link>
+                        <Link to="/Fiction" className='HeadingLink'>Fiction</Link>
+                        <Routes>
+                            <Route path='/NonFiction' Component={NonFiction}></Route>
+                            <Route path='/Edited' Component={Edited}></Route>
+                            <Route path='/Reference' Component={Reference}></Route>
+                            <Route path='/Fiction' Component={Fiction}></Route>
+                        </Routes>
+                    </Router>
+
                 </div>
-            </NavBar>
+            </div>
         </div>
     )
 }
--- src/pages/NonFiction.js
+++ src/pages/NonFiction.js
@@ -3,7 +3,8 @@ import React from 'react'
 const NonFiction = () => {
   return (
     <div>
-      
+      fyfy gyhguhgiuft dftyfhgcj<img src='https://thumbs.dreamstime.com/b/stacked-closed-books-icon-cartoon-isolated-side-view-vector-illustration-graphic-design-149755178.jpg' className='Logoimg' alt="logo"></img>
+            <h1 className='LogoName'>Kitabcopy</h1>
     </div>
   )
 }
